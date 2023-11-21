6cb155382d430b17ddc7e8db6e1d0455581740cd
Ksyusha Makusheva <meow-ksen@yandex.ru>
2023-11-21 21:18:15 +0500

    Massives part 2

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -151,6 +151,12 @@ while (u >= 0);   // условие
 // -============================================= Двумерный массив =============================================-
 
 int[,] array8 = new int[4, 5];
+
+int[,] array6 =
+{
+    { 1, 2, 3 },
+    { 3, 4, 5 }
+};
 Console.WriteLine("Length: " + array8.Length);                       // количество всех элементов
 Console.WriteLine("Rank: " + array8.Rank);                           // ранк массива
 Console.WriteLine("GetLength(0): " + array8.GetLength(0));           // количество строк
@@ -179,7 +185,7 @@ for (int i = 0; i < rows; i++)
 {
     for (int j = 0; j < columns; j++)
     {
-        Console.Write($"{array8[i, j]} \t");
+        Console.Write($"{array8[i, j]} \t"); // \t проставляет табуляцию для каждого символа одномерного массива
     }
     Console.WriteLine();
 }
@@ -191,3 +197,113 @@ foreach (int item in array)
 }
 
 
+// -============================================= Трехмерный массив =============================================-
+
+int[,,] array3D = new int[,,]
+{
+    {
+        { 1, 2, 3 },
+        { 4, 5, 6 }
+    },
+    {
+        { 7, 8, 9 },
+        { 10, 11, 12 }
+    }
+};
+
+Console.WriteLine("Length: " + array3D.Length);                       // количество всех элементов
+Console.WriteLine("Rank: " + array3D.Rank);                           // ранк массива
+Console.WriteLine("GetLength(0): " + array3D.GetLength(0));           // количество строк
+Console.WriteLine("GetUpperBound(0): " + array3D.GetUpperBound(0));   // Верхний индекс строк
+Console.WriteLine("GetLength(1): " + array3D.GetLength(1));           // количество столбцов
+Console.WriteLine("GetUpperBound(1): " + array3D.GetUpperBound(1));   // Верхний индекс столбцов
+Console.WriteLine("GetLength(2): " + array3D.GetLength(2));           // количество столбцов
+Console.WriteLine("GetUpperBound(2): " + array3D.GetUpperBound(2));   // Верхний индекс столбцов
+
+Console.WriteLine("GetLowerBound(0): " + array3D.GetLowerBound(0));   // Нижний индекс строк
+Console.WriteLine("GetLowerBound(1): " + array3D.GetLowerBound(1));   // Нижний индекс столбцов
+Console.WriteLine("GetLowerBound(2): " + array3D.GetLowerBound(2));   // Нижний индекс столбцов
+
+
+
+for (int i = 0; i <= array3D.GetUpperBound(0); i++)
+{
+    Console.WriteLine($"-========= Level {i} ==========-");
+    for (int j = 0; j <= array3D.GetUpperBound(1); j++)
+    {
+        for (int k = 0; k <= array3D.GetUpperBound(2); k++)
+        {
+            Console.Write($"{array3D[i, j, k]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"-============================-");
+}
+
+
+// -============================================= Многомерный (зубчатый) массив =============================================-
+
+int[][] numbers =
+{
+    new int[] { 1, 2 },
+    new int[] { 1, 2, 3, 4, 5 },
+    new int[] { 1, 2, 3 }
+};
+
+foreach (int[] row in numbers)
+{
+    for (int i = 0; i < row.Length; i++)
+    {
+        row[i] = new Random().Next(100);
+    }
+}
+
+for (int i = 0; i <= numbers.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= numbers[i].GetUpperBound(0); j++)
+    {
+        Console.Write($"{numbers[i][j]} \t");
+    }
+
+    Console.WriteLine();
+}
+
+
+
+// Задача. Инверсия одномерного массива
+int[] nums = { -4, 8, 10, 2, 12, 0, 5, 6 };
+
+foreach (int item in nums)
+{
+    Console.Write($"{item} \t");
+}
+Console.WriteLine();
+/*
+nums[0] <-> nums[nums.Length - 1 - 0]
+nums[1] <-> nums[nums.Length - 1 - 1]
+nums[2] <-> nums[nums.Length - 1 - 2]
+*/
+
+int n = nums.Length; // Длина массива
+int k = n / 2;
+int tmp;
+
+for (int i = 0; i < k; i++)
+{
+    tmp = nums[i];
+    nums[i] = nums[n - 1 - i];
+    nums[n - 1 - i] = tmp;
+}
+
+foreach (int item in nums)
+{
+    Console.Write($"{item} \t");
+}
+Console.WriteLine();
+
+Array.Reverse(nums);
+
+foreach (int item in nums)
+{
+    Console.Write($"{item} \t");
+}
