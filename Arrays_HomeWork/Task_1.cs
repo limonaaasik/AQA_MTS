3cd5b9def7673c1bdd259e87040a1038d3b78c5c
Ksenya Makusheva <meow-ksen@yandex.ru>
2023-11-22 17:48:28 +0500

    Added Task_1

---
+++ Arrays_HomeWork/Task_1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_HomeWork
+{
+    internal class Task_1
+    {
+        static void Main(string[] args)
+        {
+ 
+            int[] numbers = new int[] {1, 2, 3, 4};
+            Console.WriteLine("Введите число:");
+            string? num = Console.ReadLine();
+            double numDouble = Convert.ToDouble(num);
+            bool result = false;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                if (numbers[i] == numDouble)
+                {
+                    result = true;
+                }
+            }
+
+            if (result == true)
+            {
+                Console.WriteLine("Заданное число входит в массив");
+            } else
+            {
+                Console.WriteLine("Заданное число не входит в массив");
+            }
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.Write($"{numbers[i]} "); 
+            }
+
+            
+        }
+    }
+}
---
+++ Arrays_HomeWork/Task_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_HomeWork
+{
+    internal class Task_2
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("aaaaaaaaaaaaaaaaa");
+        }
+    }
+}
