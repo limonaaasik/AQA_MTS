178dd237239676244a42df4441a05fe92c682b0e
Ksenya Makusheva <meow-ksen@yandex.ru>
2023-11-30 16:48:30 +0500

    Finished Task_4

--- Arrays_HomeWork/Task_4.cs
+++ Arrays_HomeWork/Task_4.cs
@@ -35,8 +35,34 @@ namespace Arrays_HomeWork
                 Console.Write($"{i} ");
             }
 
+            Console.WriteLine();
 
+            int sumArray1 = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sumArray1 = sumArray1 + array1[i];
+            }
+            double sredArray1 = Convert.ToDouble(sumArray1) / array1.Length;
+            Console.WriteLine($"Среднее арифметическое значение массива 1: {sredArray1}");
 
+            int sumArray2 = 0;
+            for (int i = 0; i < array2.Length; i++)
+            {
+                sumArray2 = sumArray2 + array2[i];
+            }
+            double sredArray2 = Convert.ToDouble(sumArray2) / array2.Length;
+            Console.WriteLine($"Среднее арифметическое значение массива 2: {sredArray2}");
+
+            if ( sredArray1 >  sredArray2) 
+            {
+                Console.WriteLine("Среднее арфиметическое значение массива 1 > среднего арифместического значения массива 2");
+            } else if ( sredArray1 < sredArray2) 
+            {
+                Console.WriteLine("Среднее арфиметическое значение массива 2 > среднего арифместического значения массива 1");
+            } else if ( sredArray2 == sredArray1)
+            {
+                Console.WriteLine("Средние арифметические массивов 1 и 2 равны");
+            }
         }
     }
 }
