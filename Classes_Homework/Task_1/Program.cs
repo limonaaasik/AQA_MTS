2f59415a3eace66f365599ba294e35693f169d41
Ksenya Makusheva <meow-ksen@yandex.ru>
2023-12-03 15:59:52 +0500

    Finished Task_1

---
+++ Classes_Homework/Task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Classes_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone();
+            Phone phone2 = new Phone("+375-29-578-69-99", "Samsung");
+            Phone phone3 = new Phone("+7-912-639-94-89", "Xiaomi", 105.8);
+
+            // Вывод через консоль значения переменных
+            Console.WriteLine($"Значения 1 экземпляра: Номер: {phone1.number}, Модель: {phone1.model}, Вес: {phone1.weight}");
+            Console.WriteLine($"Значения 2 экземпляра: Номер: {phone2.number}, Модель: { phone2.model}, Вес: { phone2.weight}");
+            Console.WriteLine($"Значения 3 экземпляра: Номер: {phone3.number}, Модель: {phone3.model}, Вес: {phone3.weight}");
+
+            Console.WriteLine();
+
+            // Вывод через метод значения переменных
+            phone1.Print();
+            phone2.Print();
+            phone3.Print();
+
+            Console.WriteLine();
+
+            // Вызов методов receiveCall и getNumber для каждого из объектов.
+            phone1.receiveCall("Ксюша");
+            phone2.receiveCall("Даша");
+            phone3.receiveCall("Костя");
+
+            phone1.getNumber();
+            phone2.getNumber();
+            phone3.getNumber();
+
+            Console.WriteLine();
+
+
+            // Вызов метода sendMessagge
+            phone1.sendMessage("+7-912-896-78-78", "+375-29-548-69-96", "+7-961-896-87-33");
+            Console.WriteLine();
+            phone2.sendMessage("+7-912-896-78-78", "+375-29-548-69-96", "+7-961-896-87-33", "+7-916-896-00-33", "+7-961-896-85-00");
+
+        }
+    }
+}
---
+++ Classes_Homework/Task_1/Task_1.cs
@@ -0,0 +1,75 @@
+﻿
+using System.Security.Cryptography;
+
+public class Phone
+{
+    // блок переменных
+    public string number;
+    public string model;
+    public double weight;
+
+    // блок конструкторов
+    public Phone()
+    {
+
+    }
+
+    public Phone(string number, string model)
+    {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(string number, string model, double weight) : this (number, model)
+    {
+        this.weight = weight;
+    }
+
+
+    // блок методов
+    public void receiveCall(string name)
+    {
+        Console.WriteLine($"Звонит: {name}");
+    }
+
+    public string getNumber()
+    {
+        return number;
+    }
+
+    public void sendMessage(params string[] nums )
+    {
+        Console.WriteLine("Сообщение будет отправлено следующим номерам: ");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine(nums[i]);
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Значения экземпляра: Номер: {number}, Модель: {model}, Вес: {weight}");
+    }
+}
+
+
+//Задание 1:
+//Создайте класс Phone, который содержит переменные number, model и weight.
+
+//Добавить конструкторы в класс Phone: 
+//конструктор без параметров.
+//принимает на вход два параметра для инициализации переменных класса - number, model.
+//принимает на вход три параметра для инициализации всех переменных класса Phone. Вызвать из этого конструктора конструктор с двумя параметрами.
+
+//Добавить в класс Phone методы: 
+//receiveCall, с одним параметром “имя звонящего”. 
+//Этот метод выводит на консоль сообщение “Звонит {name}”. 
+//getNumber
+//Этот метод возвращает номер телефона.
+//sendMessage с аргументами переменной длины.  
+//Этот метод принимает на вход номера телефонов, которым будет отправлено сообщение. Метод выводит на консоль номера этих телефонов 
+
+//Создайте три экземпляра этого класса используя разные конструкторы.
+//Выведите на консоль значения их переменных.
+//Вызвать методы receiveCall и getNumber для каждого из объектов.
+//Вызвать метод sendMessage с 2-мя и 5-ю номерами телефонов.
