c3fd570cd0b838595f9c5a6a5b78f65f31bf5da7
Ksyusha Makusheva <meow-ksen@yandex.ru>
2023-12-04 13:09:22 +0500

    Added Task_2

---
+++ Classes_Homework/Task_2/Class CreditCard .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Homework
+{
+    public class CreditCard
+    {
+        // Блок переменных
+        public string accountNumber;
+        public double accountSum;
+
+
+        //Блок конструктора
+        public CreditCard(string accountNumber, double accountSum) 
+        {
+            this.accountNumber = accountNumber;
+            this.accountSum = accountSum;
+        }
+
+
+        // Блок методов
+        public void sendMoneyToCard(double amount)
+        {
+            accountSum += amount;
+        }
+
+        public void takeCashFromCard(double amount)
+        {
+            accountSum -= amount;
+        }
+
+        public void InfoCard()
+        {
+            Console.WriteLine($"Номер счета: {accountNumber}, Текущая сумма на счету: {accountSum}");
+        }
+    }
+}
---
+++ Classes_Homework/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard card1 = new CreditCard("8797 8989 8989 8954", 1000);
+            CreditCard card2 = new CreditCard("2222 8989 2222 1111", 2000.35);
+            CreditCard card3 = new CreditCard("5555 6666 2222 7777", 35897.06);
+
+            card1.sendMoneyToCard(1858.96);
+            card2.sendMoneyToCard(3000);
+            card3.takeCashFromCard(15000);
+
+            card1.InfoCard();
+            card2.InfoCard();
+            card3.InfoCard();
+        }
+    }
+}
