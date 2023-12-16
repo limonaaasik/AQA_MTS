1148c833bd1177e18c9c53d747c8805044331f94
Ksenya Makusheva <meow-ksen@yandex.ru>
2023-12-16 14:43:28 +0500

    Started Task1

---
+++ Task1/Authorization.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    internal class Authorization
+    {
+        public static string EnterAuthorization(string login, string password, string confirmPassword)
+        {
+            try
+            {
+                login.Length > 20 && login.Contains(" ");
+            }
+        }
+    }
+}
