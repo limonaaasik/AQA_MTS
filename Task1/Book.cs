db0f9e2d4649b37a7a0def4b786e2381414254ea
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-11 20:39:03 +0500

    add

---
+++ Task1/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Book
+    {
+        public string name;
+        public string author;
+        public int yearPublication;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название книги:{name}, Автор книги: {author}, Год публикации: {yearPublication}");
+        }
+
+    }
+}
---
+++ Task1/Library.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Library
+    {
+        ArrayList bookList = new ArrayList();
+
+        public void AddBook(Book book)
+        {
+            bookList.Add(book);
+        }
+
+        public void ShowBooks() 
+        {
+            foreach (Book book in bookList)
+            {
+                book.ShowInfo();
+            }
+
+            for (int i = 0; i < bookList.Count; i++)
+            {
+                Console.Write(i);
+                
+            }
+        }
+
+        public void ShowBooksByAuthor(string author)
+        {
+            bool bookFound = false;
+            foreach(Book book in bookList)
+            {
+                if (author == book.author)
+                {
+                    bookFound = true;
+                    book.ShowInfo();
+                }
+            }
+
+            if (!bookFound)
+            {
+                Console.WriteLine("Не найдено книг по данному автору");
+            }
+        }
+    }
+}
