c6c567f9e0ad8d6ef76654457c3b87d3ca7087bf
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-15 16:37:56 +0500

    finished Task1

---
+++ Task_1/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Point<T>
+    {
+        public T x;
+        public T y;
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void PrintInfo ()
+        {
+            Console.WriteLine($"X: {x}, Y: {y}");
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Drawing;
+using Task_1;
+
+//Реализовать класс Point, который определяет точку на координатной плоскости. В классе реализовать:
+//внутренние поля x, y;
+//конструктор с 2 параметрами;
+//свойства доступа к внутренним полям класса;
+//метод, выводящий значения внутренних полей класса.
+
+Console.WriteLine("Вывод коррдинат точек: ");
+
+Point<int> intPoint = new Point<int>(10, 20);
+intPoint.PrintInfo();
+
+Point<string> stringPoint = new Point<string>("координата 1", "координата 2");
+stringPoint.PrintInfo();
+
+Random randomPoint = new Random();
+Point<int> pointRandom = new Point<int>(randomPoint.Next(), randomPoint.Next());
+pointRandom.PrintInfo();
