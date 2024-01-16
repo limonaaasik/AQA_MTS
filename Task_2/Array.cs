3389f531632622982a510c1d923ed99d5a3ff607
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-16 19:23:42 +0500

    finished Task_2

--- Task_2/Array.cs
+++ Task_2/Array.cs
@@ -8,21 +8,39 @@ namespace Task_2
 {
     public class Array<T>
     {
-        public T[] array;
+        public T[] array = new T[0];
 
         public void Add(T value)
         {
-            var tmp = new List<T>(array);
-            tmp.Add(value);
-            array = tmp.ToArray();
+            List<T> list = new List<T>(array);
+            list.Add(value);
+            array = list.ToArray();
         }
-        public void PrintArray(T[] array)
+
+        public void Remove(int index)
+        {
+            List<T> list = new List<T>(array);
+            list.RemoveAt(index);
+            array = list.ToArray();
+        }
+
+        public void PrintArray()
         {
             for (int index = 0; index < array.Length; index++)
             {
                 Console.Write($"{array[index]} ");
             }
             Console.WriteLine();
+        }
+
+        public T GetElement (int index)
+        {
+            return array[index];
+        }
+
+        public int GetLength()
+        {
+            return array.Length;
         }
     }
 }
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,7 +1,22 @@
 ﻿using System.Collections.Generic;
 using Task_2;
 
-Array<int> strArray = new Array<int>();
-strArray.Add(1);
-strArray.Add(3);
-strArray.Add(10);
+Array<int> intArray = new Array<int>();
+intArray.Add(1);
+intArray.Add(3);
+intArray.Add(10);
+
+Console.WriteLine("Данные массива после добавления:");
+intArray.PrintArray();
+
+Console.WriteLine();
+var element = intArray.GetElement(0);
+Console.WriteLine($"Элемент по индексу 0: {element}");
+
+Console.WriteLine();
+intArray.Remove(1);
+Console.WriteLine("Данные массива после удаления элемента по индексу 1:");
+intArray.PrintArray();
+
+Console.WriteLine();
+Console.WriteLine($"Длина массива: {intArray.GetLength()}");
