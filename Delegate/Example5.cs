6f191b180be8c28c9018d33f95089139cdde507c
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-18 17:05:41 +0500

    ++

--- Delegate/Example5.cs
+++ Delegate/Example5.cs
@@ -3,45 +3,63 @@ namespace Delegate;
 public class Example5
 {
     // Тип делегата
-    public delegate void NoParameters();
+    private delegate void NoParameters();
 
     delegate int Operation(int x, int y);
 
     public void Run()
     {
-        NoParameters noPrm = delegate { Console.WriteLine("Have fun!"); };
+        NoParameters noPrm = delegate { Console.WriteLine("Have fun!!!"); };
 
         // Делегаты как параметры методов
-        Test(noPrm);
+        Console.WriteLine(Test(noPrm));
 
         // Возвращение делегатов из метода
         Operation operation = SelectOperation(OperationType.Add);
-        Console.WriteLine(operation(10, 4)); // 14
+        Console.WriteLine(operation(10, 4));
 
         operation = SelectOperation(OperationType.Subtract);
-        Console.WriteLine(operation(10, 4)); // 6
+        Console.WriteLine(operation(10, 4));
 
         operation = SelectOperation(OperationType.Multiply);
-        Console.WriteLine(operation(10, 4)); // 40
+        Console.WriteLine(operation(10, 4));
+
+        OperationType[] operationTypes = (OperationType[])Enum.GetValues(typeof(OperationType));
+
+        Operation operation1;
+        foreach (OperationType type in operationTypes)
+        {
+            operation1 = SelectOperation(type);
+            Console.WriteLine(operation1(11, 5));
+        }
     }
 
-    private bool Test(NoParameters delegatvalue)
+    private bool Test(NoParameters delegateValue)
     {
-        delegatvalue();
+        delegateValue();
+
         return true;
     }
 
     private int Add(int x, int y) => x + y;
-    private int Subtract(int x, int y) => x - y;
+    private int Subtrack(int x, int y) => x - y;
     private int Multiply(int x, int y) => x * y;
 
-    private Operation SelectOperation(OperationType opType)
+    private Operation SelectOperation(OperationType operationType)
     {
-        switch (opType)
+        switch (operationType)
         {
-            case OperationType.Add: return Add;
-            case OperationType.Subtract: return Subtract;
+            case OperationType.Add:
+                return new Operation(ShowData) + Add;
+            case OperationType.Subtract: return Subtrack;
             default: return Multiply;
         }
+    }
+
+    private int ShowData(int x, int y)
+    {
+        Console.WriteLine($"Начало операции с {x} {y}");
+
+        return 0;
     }
 }
