021885a0c5c9cd5e0d2fc99e2aa4a16515be71f7
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-21 15:44:42 +0500

    finished Tas_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿using Task_2;
+
+Radius rad = new Radius();
+
+Radius.Calculate calc = new Radius.Calculate(rad.CircleLength);
+calc += rad.CircleSquare;
+calc += rad.VolumeBall;
+calc(10);
---
+++ Task_2/Radius.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Radius
+    {
+        public delegate double Calculate(double x);
+
+        public double CircleLength(double radius)
+        {
+            double length = 2 * Math.PI * radius;
+            Console.WriteLine($"Длина окружности: {length}");
+            return length;
+        }
+        public double CircleSquare(double radius)
+        {
+            double square = Math.PI* Math.Pow(radius, 2);
+            Console.WriteLine($"Площадь круга: {square}");
+            return square;
+        }
+        public double VolumeBall(double radius)
+        {
+            double volume = 4d / 3 * Math.PI * Math.Pow(radius, 3); // d - double, указала, что число с плавающей точкой
+            Console.WriteLine($"Объем шара: {volume}");
+            return volume;
+        }
+
+    }
+}
