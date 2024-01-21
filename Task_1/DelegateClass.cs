a8a1e22945fd6b196a42d6edbc9fa7c56e91e356
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-21 14:52:26 +0500

    finished Task_1

---
+++ Task_1/DelegateClass.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_1
+{
+    internal class DelegateClass
+    {
+            // Тип делегата
+            public delegate int NoParameters();
+
+            public int RandomNumbers()
+            {
+                Random rnd = new Random();
+                return rnd.Next(0, 10);
+            }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿using Task_1;
+
+DelegateClass dg = new DelegateClass();
+DelegateClass.NoParameters noparameters = new DelegateClass.NoParameters(dg.RandomNumbers);
+Console.WriteLine(noparameters());
