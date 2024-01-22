171d0bb779c0644cdcb0d09507dd6f2afb196378
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-22 15:00:14 +0500

    classwork

--- Event/NotificationHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Event;
-
-public class NotificationHelper
-{
-    public void NotifyByEmail(string email)
-    {
-        Console.WriteLine($"Notification was send with message {email}");
-    }
-}
