c7d40abf542808611d359809e276ee3317103fae
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-23 15:20:33 +0500

    finished Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> nums = new List<int> { 1, 5, 5, 7, 8, 8, 9, 10, 12, 12, 15 };
+
+var numsResult = nums.Where(number => (number % 2 != 0)).Distinct();
+
+Console.WriteLine("Результат:");
+foreach (var item in numsResult)
+{
+    Console.WriteLine(item);
+}
