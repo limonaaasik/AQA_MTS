5f241f94780207f9a3617f2d638521184c7c3c8f
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-01-27 14:19:40 +0500

    finished Task_2

---
+++ Task_2/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Client
+    {
+        public int code;
+        public int year;
+        public int month;
+        public int trainingDuration;
+
+        public Client(int code, int year, int month, int traininDuration) 
+        {
+            code = this.code;
+            this.year = year;
+            this.month = month;
+            this.trainingDuration = traininDuration;
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Task_2;
+using static System.Net.Mime.MediaTypeNames;
+
+List<Client> clients = new List<Client>()
+{
+    new Client(0, 2022, 3, 12),
+    new Client(1, 2024, 12, 16),
+    new Client(2, 2012, 5, 32),
+    new Client(3, 2003, 8, 12),
+    new Client(4, 2010, 10, 97),
+    new Client(5, 2010, 1, 50),
+};
+
+int minDuration = clients.Min(client => client.trainingDuration);
+var result = clients.Where(client => client.trainingDuration == minDuration).Select(client =>
+new
+{
+    client.trainingDuration,
+    client.year,
+    client.month
+}).Last();
+
+Console.WriteLine($"Минимальная продолжительность занятий: {result.trainingDuration}, Год: {result.year}, Месяц: {result.month}");
+
+
+
