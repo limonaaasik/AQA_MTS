400896d8e578569bb5877f41673cab33c6fdf668
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-03 13:01:32 +0500

    finish classwork 1

---
+++ Locators/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Allure.Core;
+using OpenQA.Selenium;
+using SeleniumBasic.Core;
+
+namespace SeleniumBasic.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver = new Browser().Driver;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
