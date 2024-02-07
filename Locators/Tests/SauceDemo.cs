bb01fac1b73c7e5bd7238dbc8366678a98ad1997
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-07 16:15:31 +0500

    new changes

--- Locators/Tests/SauceDemo.cs
+++ Locators/Tests/SauceDemo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace SauceDemo.Tests
 {
-    public class SauceDemo
+    public class SauceDemo : BaseTest
     {
+        [Test]
+        public void LocatorsTest()
+        {
+            Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+
+            // Basic
+            Assert.That(Driver.FindElement(By.Id("user-name")).Displayed);
+            Assert.That(Driver.FindElement(By.Name("user-name")).Displayed);
+            Assert.That(Driver.FindElement(By.ClassName("form_column")).Displayed);
+            Assert.That(Driver.FindElement(By.TagName("input")).Displayed);
+
+        }
     }
 }
