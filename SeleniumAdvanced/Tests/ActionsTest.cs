abe05788553f81188defacd1970e8ece9d083b70
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-10 14:35:34 +0500

    done

--- SeleniumAdvanced/Tests/ActionsTest.cs
+++ SeleniumAdvanced/Tests/ActionsTest.cs
@@ -16,8 +16,8 @@ public class ActionsTest : BaseTest
 
         var targetElements = WaitsHelper.WaitForAllVisibleElementsLocatedBy(By.CssSelector(".figure"));
         actions
-            .MoveToElement(targetElements[0], 10, 10)
-            .Click(WaitsHelper.WaitForExists(By.CssSelector("[href='/users/1']")))
+            .MoveToElement(targetElements[0], 50, 50)
+            .Click(WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("[href='/users/1']")))
             .Build()
             .Perform();
 
@@ -25,21 +25,56 @@ public class ActionsTest : BaseTest
     }
 
     [Test]
+    public void DragAndDropTest()
+    {
+        Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/drag_and_drop");
+
+        var actions = new Actions(Driver);
+
+        var targetElementA = WaitsHelper.WaitForVisibilityLocatedBy(By.Id("column-a"));
+        var targetElementB = WaitsHelper.WaitForVisibilityLocatedBy(By.Id("column-b"));
+
+        actions
+            .DragAndDrop(targetElementA, targetElementB)
+            .Pause(TimeSpan.FromSeconds(3))
+            .Build()
+            .Perform();
+    }
+
+    [Test]
+    public void CheckboxTest()
+    {
+        Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/checkboxes");
+
+        var actions = new Actions(Driver);
+
+        var targetElementA = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("#checkboxes input"));
+
+        actions
+            .MoveToElement(targetElementA)
+            .Click()
+            .Pause(TimeSpan.FromSeconds(3))
+            .Build()
+            .Perform();
+    }
+
+    [Test]
     public void FileUploadTest()
     {
         Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/upload");
 
-        var fileUploadPath = WaitsHelper.WaitForExists(By.Id("file-upload"));
+        var fileUploadElement = WaitsHelper.WaitForExists(By.Id("file-upload"));
+
         // Получаем путь к исполняемому файлу (exe)
         string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         // Конструируем путь к файлу внутри проекта
         string filePath = Path.Combine(assemblyPath, "Resources", "download.jpeg");
-        Console.WriteLine(filePath);
-        
-        fileUploadPath.SendKeys(filePath);
-        
+
+        fileUploadElement.SendKeys(filePath);
+
         WaitsHelper.WaitForExists(By.Id("file-submit")).Submit();
+
         Thread.Sleep(5000);
     }
 }
