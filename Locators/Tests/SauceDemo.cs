585f434317faf70d8102ee914322481549749862
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-12 17:35:52 +0500

    finished

--- Locators/Tests/SauceDemo.cs
+++ Locators/Tests/SauceDemo.cs
@@ -14,21 +14,8 @@ namespace SauceDemo.Tests
         {
             Driver.Navigate().GoToUrl("https://www.saucedemo.com/");
 
-            
-            Assert.That(Driver.FindElement(By.Id("user-name")).Displayed);
-            Assert.That(Driver.FindElement(By.Name("user-name")).Displayed);
-            Assert.That(Driver.FindElement(By.ClassName("form_column")).Displayed);
-            Assert.That(Driver.FindElement(By.TagName("input")).Displayed);
-
-            Driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
-            Driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
-            Driver.FindElement(By.Id("login-button")).Click();
-
-            Assert.That(Driver.FindElement(By.LinkText("Sauce Labs Bike Light")).Displayed);
-            Assert.That(Driver.FindElement(By.PartialLinkText("Bike Light")).Displayed);
-
             //XPath
-            Assert.That(Driver.FindElement(By.XPath("//input[@placeholder='Password']")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//div/input[@placeholder='Password']")).Displayed);
             Assert.That(Driver.FindElement(By.XPath("//*[text()='secret_sauce']")).Displayed);
             Assert.That(Driver.FindElement(By.XPath("//*[contains(@data-test,'pass')]")).Displayed);
             Assert.That(Driver.FindElement(By.XPath("//*[contains(text(),'standard_')]")).Displayed);
@@ -44,15 +31,27 @@ namespace SauceDemo.Tests
             Assert.That(Driver.FindElement(By.CssSelector(".submit-button.btn_action")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector(".login-box .input_error")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("#login_button_container")).Displayed);
-            Assert.That(Driver.FindElement(By.CssSelector("br")).Displayed);
+            Assert.That(Driver.FindElement(By.CssSelector("h4")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("div.login_password")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("[placeholder='Username']")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("input[name~='user-name']")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("[class|='login']")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("[autocorrect^='of']")).Displayed);
             Assert.That(Driver.FindElement(By.CssSelector("[autocorrect$='ff']")).Displayed);
-            Assert.That(Driver.FindElement(By.CssSelector("[type*='tex']")).Displayed);
+            Assert.That(Driver.FindElement(By.CssSelector("[class*=_]")).Displayed);
 
+            //Basic
+            Assert.That(Driver.FindElement(By.Id("user-name")).Displayed);
+            Assert.That(Driver.FindElement(By.Name("user-name")).Displayed);
+            Assert.That(Driver.FindElement(By.ClassName("form_column")).Displayed);
+            Assert.That(Driver.FindElement(By.TagName("input")).Displayed);
+
+            Driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            Driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            Driver.FindElement(By.Id("login-button")).Click();
+
+            Assert.That(Driver.FindElement(By.LinkText("Sauce Labs Bike Light")).Displayed);
+            Assert.That(Driver.FindElement(By.PartialLinkText("Bike Light")).Displayed);
         }
     }
 }
