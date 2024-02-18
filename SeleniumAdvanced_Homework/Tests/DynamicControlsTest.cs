330de22e4b7fba958a728e0ca2afbfa46fa7862d
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-18 22:21:55 +0500

    finished task 2

---
+++ SeleniumAdvanced_Homework/Tests/DynamicControlsTest.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvanced_Homework.Tests
+{
+    public class DynamicControlsTest : BaseTest
+    {
+        public void DynamicControls()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
+
+            // Нахожу чекбокс
+            var checkbox = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("input[type='checkbox']"));
+            // Ищу поля ввода
+            var inputField = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("input[type= 'text']"));
+            // Ищу кнопку Enabled
+            var buttonEnable = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("button[onclick='swapCheckbox()']"));
+            // Ищу кнопку Remove, кликаю по кнопке
+            var buttonRemove = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("button[onclick='swapCheckbox()']"));
+            buttonRemove.Click();
+
+            // Проверяю, что элемент "It's gone!" отобразилсяи и корректен
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.XPath("//*[contains(text(),'gone!')]")).Text, Is.EqualTo("It's gone!"));
+
+            // Проверяю, что чекбокса нет
+            Assert.That(WaitsHelper.WaitForElementInvisible(checkbox));
+
+            // Проверяю, что поле ввода disabled
+            Assert.That(WaitsHelper.WaitForElementInvisible(inputField));
+
+            // Кликаю по кнопке Enabled
+            buttonEnable.Click();
+
+            // Проверяю, что элемент "It's enabled!" отобразилсяи и корректен
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.XPath("//*[contains(text(),'enabled!')]")).Text, Is.EqualTo("It's enabled!"));
+
+            // Проверяю, что поле Enabled
+            Assert.That(inputField.Enabled);
+        }
+
+    }
+}
