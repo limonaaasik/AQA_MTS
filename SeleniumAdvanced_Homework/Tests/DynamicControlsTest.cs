2f16505012d1c97793f150aaa8a75a5df482f2bd
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-18 22:22:05 +0500

    ++

--- SeleniumAdvanced_Homework/Tests/DynamicControlsTest.cs
+++ SeleniumAdvanced_Homework/Tests/DynamicControlsTest.cs
@@ -9,6 +9,7 @@ namespace SeleniumAdvanced_Homework.Tests
 {
     public class DynamicControlsTest : BaseTest
     {
+        [Test]
         public void DynamicControls()
         {
             Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/dynamic_controls");
@@ -18,25 +19,26 @@ namespace SeleniumAdvanced_Homework.Tests
             // Ищу поля ввода
             var inputField = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("input[type= 'text']"));
             // Ищу кнопку Enabled
-            var buttonEnable = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("button[onclick='swapCheckbox()']"));
+            var buttonEnable = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("button[onclick='swapInput()']"));
+            
             // Ищу кнопку Remove, кликаю по кнопке
             var buttonRemove = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("button[onclick='swapCheckbox()']"));
             buttonRemove.Click();
 
             // Проверяю, что элемент "It's gone!" отобразилсяи и корректен
-            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.XPath("//*[contains(text(),'gone!')]")).Text, Is.EqualTo("It's gone!"));
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message")).Text, Is.EqualTo("It's gone!"));
 
             // Проверяю, что чекбокса нет
             Assert.That(WaitsHelper.WaitForElementInvisible(checkbox));
 
             // Проверяю, что поле ввода disabled
-            Assert.That(WaitsHelper.WaitForElementInvisible(inputField));
+            Assert.That(!inputField.Enabled);
 
             // Кликаю по кнопке Enabled
             buttonEnable.Click();
 
             // Проверяю, что элемент "It's enabled!" отобразилсяи и корректен
-            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.XPath("//*[contains(text(),'enabled!')]")).Text, Is.EqualTo("It's enabled!"));
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message")).Text, Is.EqualTo("It's enabled!"));
 
             // Проверяю, что поле Enabled
             Assert.That(inputField.Enabled);
