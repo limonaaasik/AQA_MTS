0928d5b5548aaf77478f0efd251ebe047a41a501
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-21 14:27:05 +0500

    +++

--- SeleniumAdvanced/Helpers/Configuration/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace SeleniumAdvanced_Homework.Helpers.Configuration;
-
-public class AppSettings
-{
-    public string? URL { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
-}
