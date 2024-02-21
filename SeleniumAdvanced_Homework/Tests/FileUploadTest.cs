bdec9a13a271f2f6cf92e12ebffa08fd56365bb7
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-21 14:26:04 +0500

    finished HW

---
+++ SeleniumAdvanced_Homework/Tests/FileUploadTest.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvanced_Homework.Tests
+{
+    public class FileUploadTest : BaseTest
+    {
+        [Test]
+        public void FileCheckNameTest()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/upload");
+
+            var fileUploadElement = WaitsHelper.WaitForExists(By.Id("file-upload"));
+
+            // Получаем путь к исполняемому файлу
+            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Конструируем путь к файлу внутри проекта
+            string filePath = Path.Combine(assemblyPath, "Resources", "cat_picture.jpg");
+
+            fileUploadElement.SendKeys(filePath);
+
+            WaitsHelper.WaitForExists(By.Id("file-submit")).Submit();
+
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("uploaded-files")).Text, Is.EqualTo("cat_picture.jpg"));
+        }
+    }
+}
