dde23ba42342dfb4f82357f4f917fdf988a426f5
Ksenya Makusheva <meow-ksen@yandex.ru>
2024-02-27 17:56:49 +0500

    ++

--- PageObject_HW/Pages/SecondCheckOutPage.cs
+++ PageObject_HW/Pages/SecondCheckOutPage.cs
@@ -9,18 +9,36 @@ namespace PageObject_HW.Pages
 {
     public class SecondCheckOutPage : BasePage
     {
+        private static string END_POINT = "checkout-step-two.html";
+
+        // Описание элементов
+        private static readonly By TitleLabelBy = By.ClassName("title");
+        private readonly By ProductBackpackBy = By.ClassName("inventory_item_name");
+        private readonly By SummaryTotalLabelBy = By.CssSelector("[class~='summary_total_label']");
+        private readonly By FinishButtonBy = By.Id("finish");
+
+        // Инициализация класса + переопределение
         public SecondCheckOutPage(IWebDriver driver, bool openPageByUrl = false) : base(driver, openPageByUrl)
         {
         }
 
         public override bool IsPageOpened()
         {
-            throw new NotImplementedException();
+            return TitleLabel.Text.Trim().Equals("Checkout: Overview");
         }
 
         protected override string GetEndpoint()
         {
-            throw new NotImplementedException();
+            return END_POINT;
         }
+
+        // Методы
+        public IWebElement TitleLabel => WaitsHelper.WaitForExists(TitleLabelBy);
+        public IWebElement ProductBackpack => WaitsHelper.WaitForExists(ProductBackpackBy);
+        public IWebElement SummaryTotalLabel => WaitsHelper.WaitForExists(SummaryTotalLabelBy);
+        public IWebElement FinishButton => WaitsHelper.WaitForExists(FinishButtonBy);
+        public bool FinishButtonVisible => WaitsHelper.WaitForVisibility(FinishButton);
+
+        
     }
 }
